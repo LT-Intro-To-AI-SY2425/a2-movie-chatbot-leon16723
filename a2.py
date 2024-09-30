6acc68c0489a4debad1998e7005188880001639a
Leon16723 <lscurtu@cps.edu>
2024-09-30 11:26:34 -0500

    done

--- a2.py
+++ a2.py
@@ -24,36 +24,44 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
     while pind < len(pattern) or sind < len(source):
         # your job is to fill out the body of this loop
 
-
         # 1) if we reached the end of the pattern but not source
-        if pind == len(pattern) and  sind < len(source):
+        if pind == len(pattern) and sind != len(source):
             return None
+
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
         elif pattern[pind] == '%':
-            if pind + 1 == len(pattern):
-                combined = ' '.join(source[sind:])
-                result.append(combined)
+            if pind == len(pattern) - 1:
+                res = " ".join(source[sind:])
+                result.append(res)
+                # pind += 1
+                # sind = len(source)
                 return result
             else:
-                return None # placeholder, fix later
-            
+                pind += 1
+                accum = ""
+                while pattern[pind] != source[sind]:
+                    accum += source[sind] + " "
+                    sind += 1
+                    if sind == len(source):
+                        return None
+                result.append(accum.rstrip())
         # 3) if we reached the end of the source but not the pattern
-        elif sind == len(source) and pind < len(pattern):
+        elif sind == len(source):
             return None
         # 4) if the current thing in the pattern is an _
         elif pattern[pind] == '_':
             result.append(source[sind])
             pind += 1
             sind += 1
-
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
-        elif source[sind] == pattern[pind]:
+        elif pattern[pind] == source[sind]:
             pind += 1
             sind += 1
+
         # 6) else : this will happen if none of the other conditions are met it
         # indicates the current thing it pattern doesn't match the current thing in
         # source
@@ -86,5 +94,7 @@ if __name__ == "__main__":
     # this last case is a strange one, but it exposes an issue with the way we've
     # written our match function
     assert match(["x", "%", "z"], ["x", "y", "z", "z", "z"]) == None, "test 15 failed"
+    assert match(["_", "%", "a"], ["x", "y", "z", "b", "c", "d"]) == None, "test 16 failed"
+
 
     print("All tests passed!")
